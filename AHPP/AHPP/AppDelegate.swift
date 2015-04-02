0ea27c1a91e9e309f20f61feb6e2fc2e272e8487
Andrew Gable <andrewgable@u.boisestate.edu>
2015-04-01 18:48:06 -0600

    Added function to run csv

--- AHPP/AHPP/AppDelegate.swift
+++ AHPP/AHPP/AppDelegate.swift
@@ -19,6 +19,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: NSString?, annotation: AnyObject) -> Bool {
+        
+        // Prints the csv file url
+        print(url)
+        
+        return true
+    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
